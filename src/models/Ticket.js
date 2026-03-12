d050714954a63385a06e15d9b52ea0541be39dd9
Nabin <sharmanabin.0910@gmail.com>
2026-03-12 23:18:18 +0530

    Model for my ticket

---
+++ src/models/Ticket.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const TicketSchema = new mongoose.schema({
+    ticketId : {
+        type : String,
+        required : true ,
+        unique : true
+    },
+    issueType : {
+        type : String ,
+        required : true
+    },
+    location : {
+        type : String ,
+        required : true
+    },
+    description : {
+        type : String
+    },
+    callerName : {
+        type : String,
+    },
+    status : {
+        type : String,
+        default : 'Open'
+    },
+    createdAt : {
+        type : String,
+        default : Date.now()
+    }
+});
+
+export default mongoose.model('Ticket', TicketSchema);
